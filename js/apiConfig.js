49f864f20e8ce6b8e005f795217213416079f1cf
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-06-28 23:31:14 +0300

    Fix API_BASE_URL by removing duplicate slash in the endpoint

--- js/apiConfig.js
+++ js/apiConfig.js
@@ -1,6 +1,6 @@
 // ملف إعداد رابط الـ API الرئيسي
 // export const API_BASE_URL = 'http://localhost:5000/api';
-export const API_BASE_URL = 'https://olive-oil-store-api.vercel.app//api';
+export const API_BASE_URL = 'https://olive-oil-store-api.vercel.app/api';
 
 
 // دالة مساعدة لبناء الروابط بسهولة
